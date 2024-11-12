0495b2d4048d6abfd3f0d439000f90dd46778011
zHElEARN <personal@zhelearn.com>
2024-11-12 17:16:26 +0800

    feat: add animation for avatar entrance display

--- creamakers/src/components/LogoCircle/index.js
+++ creamakers/src/components/LogoCircle/index.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 
 const LogoCircle = ({ logo, avatars }) => {
+    const [animationDelay, setAnimationDelay] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setAnimationDelay(false), 100);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div className="circle-container">
             <div className="center-logo" style={{ backgroundImage: `url(${logo})` }}></div>
@@ -9,7 +16,7 @@ const LogoCircle = ({ logo, avatars }) => {
             {avatars.map((logo, index) => (
                 <div
                     key={index}
-                    className="logo-item"
+                    className={`logo-item ${animationDelay ? 'initial-position' : 'final-position'}`}
                     style={{
                         backgroundImage: `url(${logo})`,
                         transform: `rotate(${index * (360 / avatars.length)}deg) translate(150px) rotate(-${index * (360 / avatars.length)}deg)`,
